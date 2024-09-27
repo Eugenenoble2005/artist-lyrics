e172ee615cdd1ea88ab3c43d342e61cf1cbb5d1c
Noble Eugene <nobleeugene2005@Gmail.com>
2024-09-27 11:08:29 +0100

    fixed script not getting correct artist from args

--- main.js
+++ main.js
@@ -3,18 +3,20 @@ const Client = new Genius.Client(process.env.GENIUS_API_KEY); // Scrapes if no k
 (() => {
     try {
         const args = process.argv;
+        var index = -1;
         args.forEach(async (arg)=>{
+            index++;
             if(arg.startsWith("--artist")){
-                var artist = arg.split("=")[0]
+                var artist = process.argv[index+1];
+                console.log(artist);
                 const songs = await Client.songs.search(artist);
                 const indexSongs = Math.floor(Math.random() * Math.floor(songs.length));
                 const lyrics = await songs[indexSongs].lyrics();
                 const title = songs[indexSongs].fullTitle;
                 const { image } = songs[indexSongs];
-                
                 const arrLyrics = lyrics.split('\n').filter((l) => l.length && l[0] !== '[');
                 const indexLyrics = Math.floor(Math.random() * Math.floor(arrLyrics.length));
-                console.log(arrLyrics[indexLyrics]);
+                console.log(arrLyrics[indexLyrics] + "--" + title);
             }
         });
       
